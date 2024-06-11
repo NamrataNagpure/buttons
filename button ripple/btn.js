ba1f0ca166dd66c1d80d3e3ab730058b67ca6c9c
Namrata Nagpure <147522872+NamrataNagpure@users.noreply.github.com>
2024-06-11 10:36:09 +0530

    Add files via upload

---
+++ button ripple/btn.js
@@ -0,0 +1,8 @@
+const btnEl = document.querySelector(".btn");
+btnEl.addEventListener("mouseover",(event)=>{
+    const x= (event.pageX-btnEl.offsetLeft);
+    const Y= (event.pageY-btnEl.offsetTop);
+    btnEl.computedStyleMap.setProperty("--xPos",x+"px")
+    btnEl.computedStyleMap.setProperty("--yPos",Y+"px")
+
+});
